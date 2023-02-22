a7de00387ba2efc43588d782ef522f0c54f48f8a
Jonathas Sardinha <jonathas.sardinha@gmail.com>
2023-02-22 15:06:38 -0300

    added localComponents folder

---
+++ src/app/localComponents/Tabs.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Tab } from "@headlessui/react";
+import { ReactNode } from "react";
+import { Results } from "./Results";
+
+function StyledTab(props: { children: ReactNode }) {
+  return (
+    <Tab
+      {...props}
+      className="transition-all duration-300 outline-none py-2 px-4 rounded-lg flex-1 ui-selected:shadow-lg ui-selected:text-orange-500 ui-selected:bg-white"
+    />
+  );
+}
+
+export function Tabs() {
+  return (
+    <Tab.Group defaultIndex={0}>
+      <Tab.List className="flex gap-4 p-2 bg-gray-100 rounded-xl mb-4">
+        <StyledTab>Geral</StyledTab>
+        <StyledTab>Categorias</StyledTab>
+      </Tab.List>
+      <Tab.Panels className="flex-1 overflow-hidden flex flex-col">
+        <Tab.Panel className="flex flex-col overflow-hidden">
+          <div className="flex flex-col justify-between h-full gap-4 overflow-auto">
+            <Results title="Guardado esse mês" expected={0} real={-100} />
+            <Results title="Entradas" expected={0} real={100} />
+            <Results title="Saídas" expected={0} real={100} />
+          </div>
+        </Tab.Panel>
+        <Tab.Panel className="flex flex-col overflow-hidden">
+          <div className="flex flex-col overflow-hidden">
+            <div>header</div>
+            <div className="overflow-auto bg-gray-100">
+              <div style={{ height: "500px" }}></div>
+            </div>
+          </div>
+        </Tab.Panel>
+      </Tab.Panels>
+    </Tab.Group>
+  );
+}
+
+/*
+
+<div className="flex flex-1 flex-col justify-between pt-10 ">
+				<Results title="Guardado esse mês" expected={0} real={-100} />
+				<Results title="Entradas" expected={0} real={100} />
+				<Results title="Saídas" expected={0} real={100} />
+			</div>
+			*/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-import { Balance } from "./components/Balance";
-import { Results } from "./components/Results";
-import { Tabs } from "./components/Tabs";
+import { Balance } from "./localComponents/Balance";
+import { Results } from "./localComponents/Results";
+import { Tabs } from "./localComponents/Tabs";
 
 export default function Home() {
-	return (
-		<main className="flex flex-1 flex-col gap-8 px-6 py-10 overflow-hidden">
-			<div className="flex justify-between">
-				<Balance title="Saldo Inicial" amount={0} />
-				<div className="border-l border-gray-400" />
-				<Balance accentColor title="Saldo Final" amount={320250.12} />
-			</div>
+  return (
+    <main className="flex flex-1 flex-col p-6 overflow-hidden">
+      <div className="flex justify-between mb-6">
+        <Balance title="Saldo Inicial" amount={0} />
+        <div className="border-l border-gray-400" />
+        <Balance accentColor title="Saldo Final" amount={320250.12} />
+      </div>
 
-			<Tabs />
-		</main>
-	);
+      <Tabs />
+    </main>
+  );
 }
